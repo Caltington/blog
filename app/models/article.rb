4ed620bb9feaba1b804118e1b5837754077114fa
Sean galvin <sean@autumnleafdigital.co.uk>
2024-03-15 12:30:11 +0000

    Comments finally done

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,10 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10}
 
+  VALIDATE_STATUSES = ['public', 'private', 'archived']
+
+  validates :status, inclusion: {in VALIDATE_STATUSES }
+  def archived?
+    status == 'archived'
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,11 @@
 class Comment < ApplicationRecord
   belongs_to :article
+
+  VALIDATE_STATUSES = ['public', 'private', 'archived']
+
+  validates :status, :inclusion: {in: VALIDATE_STATUSES}
+  
+  def archived?
+    status == 'archived'
+  end
 end
