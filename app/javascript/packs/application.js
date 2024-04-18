63e8a86a655458536626be0e307713d028660af9
Caltington <145353115+Caltington@users.noreply.github.com>
2024-04-18 09:59:16 +0100

    Turbo is scary
    aaaaaa

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,12 +4,9 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+import { Turbo } from "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "@hotwired/turbo-rails"
-
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
-Turbo.setProgressBarDelay(0)
+Turbo.setProgressBarDelay(10)
